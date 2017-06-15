a6a97bc0af19906e86955eb4e9c4927c5b4cb4b6
m1nam1 <info@minami.xyz>
2017-06-15 16:04:24 +0900

    Implement "All OFF" function

--- src/containers/Room.js
+++ src/containers/Room.js
@@ -6,7 +6,8 @@ import {
   View,
   StatusBar,
   Text,
-  Alert
+  Alert,
+  ToastAndroid
 } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
@@ -17,7 +18,7 @@ import DeviceList from '../containers/DeviceList';
 
 const isAndroid = Platform.OS === 'android'; // bool
 
-const MainButton = MKButton.coloredButton()
+const DeviceButton = MKButton.coloredButton()
   .withBackgroundColor(MKColor.Amber)
   .withStyle({
     width: 200,
@@ -38,6 +39,16 @@ const AccentButton = MKButton.accentColoredButton()
   .build();
 
 export default class Room extends Component {
+  async _getToken() {
+    console.info('_getToken()');
+    try {
+      return await AsyncStorage.getItem('AccessToken');
+    } catch (err) {
+      console.log('Error retrieving data:', err);
+      if (isAndroid) ToastAndroid.show('access token を取得できませんでした', ToastAndroid.SHORT);
+    }
+  }
+
   // TODO: implement
   _preset() {
     console.info('_preset()');
@@ -48,20 +59,73 @@ export default class Room extends Component {
         {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
         {text: 'OK', onPress: () => console.log('OK Pressed')},
       ]
-    )
+    );
   }
 
-  // TODO: implement
-  _allOff() {
+  /**
+   * 指定された部屋の全デバイスの電源を切る
+   * @param {string} room_name - 指定する部屋名
+   */
+  _allOff(room_name) {
     console.info('_allOff()');
     Alert.alert(
       'All OFF',
       'この部屋の全てのエアコンと電気の電源をオフにしますか？',
       [
-        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: 'OK', onPress: () => console.log('OK Pressed')},
+        {text: 'Cancel', onPress: () => console.log('Canceled'), style: 'cancel'},
+        {text: 'OK', onPress: () => {
+          this._getToken().then(access_token => {
+            const device_type_list = ['Air Conditioner', 'Light'];
+            for (device_type of device_type_list) {
+              switch (device_type) {
+                case 'Air Conditioner':
+                  const device_list = config.ucode.ac[room_name];
+                  for (let key in device_list) {
+                    if (device_list.hasOwnProperty(key)) {
+                      const device_id = device_list[key];
+                      this._powerOff('air_conditioners', device_id, access_token);
+                    }
+                  }
+                  break;
+                case 'Light':
+                  const device_id = config.ucode.light[room_name][`LightRoom${room_name}`];
+                  this._powerOff('lights', device_id, access_token);
+                  break;
+                default:
+                  console.error('Undefined device type:', device_type);
+              }
+            }
+            if (isAndroid) ToastAndroid.show('送信しました', ToastAndroid.SHORT);
+          });
+        }}
       ]
-    )
+    );
+  }
+
+  /**
+   * 指定されたデバイスの電源を切る
+   * @param {string} device_type - 指定するデバイスの種類
+   * @param {string} device_id - 指定するデバイスのID
+   * @param {string} access_token - アクセストークン
+   */
+  _powerOff(device_type, device_id, access_token) {
+    console.info(`_powerOff(${device_id})`);
+    const url = `${config.server}/${device_type}/${device_id}/state`;
+    console.log('url:', url);
+    const data = { id: device_id, power: 0 };
+    console.log('data:', data);
+    return fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-UIDC-Authorization-Token': access_token
+      },
+      body: JSON.stringify({ ...data })
+    })
+      .then(response => response.json())
+      .then(json => console.log('response', json))
+      .catch(err => console.error(err));
   }
 
   render() {
@@ -71,15 +135,15 @@ export default class Room extends Component {
         <AccentButton onPress={() => this._preset()}>
           <Text style={styles.btn_txt}>Preset</Text>
         </AccentButton>
-        <AccentButton onPress={() => this._allOff()}>
+        <AccentButton onPress={() => this._allOff(this.props.title)}>
           <Text style={styles.btn_txt}>All OFF</Text>
         </AccentButton>
-        <MainButton onPress={() => Actions.device_list({title: 'Air Conditioner', room: this.props.title})}>
+        <DeviceButton onPress={() => Actions.device_list({title: 'Air Conditioner', room: this.props.title})}>
           <Text style={styles.btn_txt}>Air Conditioner</Text>
-        </MainButton>
-        <MainButton onPress={() => Actions.device_list({title: 'Light', room: this.props.title})}>
+        </DeviceButton>
+        <DeviceButton onPress={() => Actions.device_list({title: 'Light', room: this.props.title})}>
           <Text style={styles.btn_txt}>Light</Text>
-        </MainButton>
+        </DeviceButton>
       </View>
     );
   }
